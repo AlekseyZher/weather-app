bd02a8c04b3ede5f146f16cec2e700ce6b4c9bfe
Aleksey <alekszher@mail.ru>
2024-07-31 14:32:58 +0600

    Refactoring JS

--- index.js
+++ index.js
@@ -1,61 +1,65 @@
 const apiKey = "5027116587b048b385a130941242807";
 
-// Получаем название города
-
 const formNode = document.querySelector("#form");
 const inputNode = document.querySelector("#input");
 const header = document.querySelector(".header");
 
-// Слушаем отправку формы
-
-formNode.onsubmit = function (e) {
-  // Отмена отправки формы
-  e.preventDefault();
-  let city = "";
-  city = inputNode.value.trim();
-
-  const error = document.querySelector(".err");
-  if (error) {
-    error.remove();
-  }
+function removeCard() {
   const prevCard = document.querySelector(".card");
   if (prevCard) {
     prevCard.remove();
   }
+}
 
-  //   Делаем запрос на сервер
+function showMessage() {
+  const error = `<div class='card'>Такого города не существует</div>`;
+  header.insertAdjacentHTML("afterend", error);
+}
+
+function showCard({ name, country, temp, condition }) {
+  const html = /*html*/ `
+  <div class="card">
+   <h2 class="card-city">${name} <span>${country}</span></h2>
+   <div class="card-weather">
+    <div class="card-value">${temp}<span>&degC</span></div>
+    <img class="card-img" src="" alt="weather" />
+   </div>
+   <div class="card-desc">${condition}</div>
+ </div>`;
+
+  header.insertAdjacentHTML("afterend", html);
+}
+
+async function getWeather(city) {
   const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=ru`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
 
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      if (data.error) {
-        header.insertAdjacentHTML(
-          "afterend",
-          `<h3 class='err'>Такого города не существует</h3>`
-        );
-      }
-      //   console.log(data);
-      //   console.log(data.current.condition.text);
-      //   console.log(data.current.temp_c);
-      //   console.log(data.location.name);
-      //   console.log(data.location.country);
+// Слушаем отправку формы
 
-      //   Отображаем полученные данные в карточке
-      const html = /*html*/ `
-        <div class="card">
-         <h2 class="card-city">${data.location.name} <span>${data.location.country}</span></h2>
-         <div class="card-weather">
-          <div class="card-value">${data.current.temp_c}<span>&degC</span></div>
-          <img class="card-img" src="" alt="weather" />
-         </div>
-         <div class="card-desc">${data.current.condition.text}</div>
-       </div>`;
+formNode.onsubmit = async function (e) {
+  // Отмена отправки формы
+  e.preventDefault();
+  // Удаляем старые карточки
+  removeCard();
+  // Получаем название города
+  let city = inputNode.value.trim();
+  // Получаем данные с сервера
+  const data = await getWeather(city);
+  // Отображаем данные
+  if (data.error) {
+    showMessage();
+  } else {
+    const weatherData = {
+      name: data.location.name,
+      country: data.location.country,
+      temp: data.current.temp_c,
+      condition: data.current.condition.text,
+    };
 
-      header.insertAdjacentHTML("afterend", html);
-    });
-
+    showCard(weatherData);
+  }
   inputNode.value = "";
 };
